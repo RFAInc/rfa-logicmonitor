b38ba8f3cefc4d5c1ae90f0e996af55e1a62f2db
Tony Pagliaro <tony@pagliaro.co>
2021-03-24 15:43:10 -0400

    Created a script that will generate the CSV file

---
+++ scripts/rfa/New-CsvImportFile.ps1
@@ -0,0 +1,42 @@
+#Requires -Modules PsGuiTools, PsCwAutomateDB
+
+$CompanyRecords = @()
+
+# Get data from DB
+$Companies = Invoke-RfaLtSqlCommand "Select Company,ExternalID FROM clients" |
+    Where-Object {$_.Company} | Sort-Object Company
+
+# Loop until cancel
+$strCompany = 'not null'
+for ($i=1; $strCompany; $i++) {
+
+    # Prompt for list of companies
+    $strCompany = $Companies.Company |
+        Show-GuiSelectItemFromList -Title 'Select a company (cancel to exit loop)'
+
+    if ($strCompany) {
+
+        # Filter by chosen item
+        $thisCompany = $Companies |
+            Where-Object {$_.Company -eq $strCompany}
+
+        # Get Locations (Not New Computers)
+        $Locations = Invoke-RfaLtSqlCommand "SELECT Name FROM locations WHERE clientid = (
+            SELECT clientid FROM clients WHERE ExternalID=$($thisCompany.ExternalID)
+        )" | Select-Object -ExpandProperty Name
+        
+        # Add to array
+        $CompanyRecords += [PSCustomObject]@{
+            companyID = $thisCompany.ExternalID
+            companyName = $thisCompany.Company
+            companyLocations = ($Locations -join ';')
+        }
+    }
+
+}
+
+# Export array as file
+$InitialDirectory = Join-Path $env:USERPROFILE 'Downloads'
+$Path = Show-GuiFilePicker -InitialDirectory $InitialDirectory -Extension 'csv' -Single -OutString
+$CompanyRecords | Export-Csv $Path -NoTypeInfo
+Get-Item $Path
